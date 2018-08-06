eeb8029e82d8b5758cf63dc7cfe798da663e01ac
Lajos Nyeki <lajos_nyeki@epam.com>
2018-08-06 12:18:59 +0200

    Reflection replaced with delegation for injecting viewmodels.

--- app/src/main/java/com/epam/nyekilajos/archcomppoc/inject/DaggerFragmentWithViewModel.kt
+++ app/src/main/java/com/epam/nyekilajos/archcomppoc/inject/DaggerFragmentWithViewModel.kt
@@ -1,36 +1,33 @@
 package com.epam.nyekilajos.archcomppoc.inject
 
-import android.os.Bundle
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.*
 import dagger.android.support.DaggerFragment
 import javax.inject.Inject
-import kotlin.reflect.KMutableProperty
-import kotlin.reflect.full.declaredMemberProperties
-import kotlin.reflect.jvm.javaType
+import kotlin.reflect.KClass
+import kotlin.reflect.KProperty
 
 open class DaggerFragmentWithViewModel : DaggerFragment() {
 
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        this.javaClass.kotlin.declaredMemberProperties.filter {
-            it.annotations.find { annotation -> annotation.annotationClass == InjectViewModel::class } != null
-        }.forEach {
-            if (it is KMutableProperty<*>) {
-                @Suppress("UNCHECKED_CAST")
-                val vm = ViewModelProviders.of(this, viewModelFactory).get(it.returnType.javaType as Class<out ViewModel>)
-                it.setter.call(this, vm)
-            }
-        }
-    }
 }
 
-@Target(AnnotationTarget.FIELD, AnnotationTarget.PROPERTY)
-@Retention(AnnotationRetention.RUNTIME)
-@MustBeDocumented
-annotation class InjectViewModel
+inline fun <reified T : ViewModel> daggerViewModel(fragment: DaggerFragmentWithViewModel) = DaggerViewModelDelegate(fragment, T::class)
 
+class DaggerViewModelDelegate<T : ViewModel>(private val fragment: DaggerFragmentWithViewModel, private val kClass: KClass<T>) : DefaultLifecycleObserver {
+
+    private var viewModel: T? = null
+
+    init {
+        fragment.lifecycle.addObserver(this)
+    }
+
+    override fun onCreate(owner: LifecycleOwner) {
+        viewModel = ViewModelProviders.of(fragment, fragment.viewModelFactory).get(kClass.java)
+    }
+
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): T {
+        return viewModel ?: throw IllegalStateException("Activity is not started yet.")
+    }
+}
--- app/src/main/java/com/epam/nyekilajos/archcomppoc/ui/adresslist/AddressListFragment.kt
+++ app/src/main/java/com/epam/nyekilajos/archcomppoc/ui/adresslist/AddressListFragment.kt
@@ -6,12 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import com.epam.nyekilajos.archcomppoc.R
 import com.epam.nyekilajos.archcomppoc.inject.DaggerFragmentWithViewModel
-import com.epam.nyekilajos.archcomppoc.inject.InjectViewModel
+import com.epam.nyekilajos.archcomppoc.inject.daggerViewModel
 
 class AddressListFragment : DaggerFragmentWithViewModel() {
 
-    @InjectViewModel
-    lateinit var viewModel: AddressListViewModel
+    private val viewModel: AddressListViewModel by daggerViewModel(this)
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         return inflater.inflate(R.layout.adress_list_fragment, container, false)
