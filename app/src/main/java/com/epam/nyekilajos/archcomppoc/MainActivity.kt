94d91d3e00e157ebcfdb0d936a7cada190a85efb
Lajos Nyeki <lajos_nyeki@epam.com>
2018-08-07 10:49:07 +0200

    Minor style fixes.

--- app/src/main/java/com/epam/nyekilajos/archcomppoc/MainActivity.kt
+++ app/src/main/java/com/epam/nyekilajos/archcomppoc/MainActivity.kt
@@ -1,19 +1,21 @@
 package com.epam.nyekilajos.archcomppoc
 
 import android.os.Bundle
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
-import com.epam.nyekilajos.archcomppoc.ui.adresslist.AddressListFragment
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
 import dagger.android.support.DaggerAppCompatActivity
 
 class MainActivity : DaggerAppCompatActivity() {
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        navController = findNavController(R.id.nav_host_fragment)
+        setupActionBarWithNavController(this, navController)
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        return findNavController(R.id.nav_host_fragment).navigateUp()
-    }
+    override fun onSupportNavigateUp(): Boolean = navController.navigateUp()
 }
